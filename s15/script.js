9706965eee3575dc4d44e4aeda04ae2c47db55cc
Mgtsampayan <90001725+Mgtsampayan@users.noreply.github.com>
2022-11-12 22:52:20 +0800

    Add files via upload

---
+++ s15/script.js
@@ -0,0 +1,23 @@
+// Question number 1
+const a = 5, b = 2, c = 'hello';
+
+// equal to operator
+console.log(a == 5);     // true
+console.log(b == '2');   // true
+console.log(c == 'Hello');  // false
+
+
+// question number 2
+const gemuel = 1;  // if i input on const 
+
+console.log(gemuel <= 3) // true
+console.log(gemuel >= 2); // false
+
+
+
+// Question number 3
+const A = 2, B = 'hello';
+
+console.log(A !== 2); // false
+console.log(A !== '2'); // true
+console.log(B !== 'Hello'); // true
